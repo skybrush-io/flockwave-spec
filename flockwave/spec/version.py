dc7cf7858c588f417f278130fd7fbbf6a528164e
Tamas Nepusz <ntamas@gmail.com>
2019-11-05 15:02:51 +0100

    bumped version to 0.20.1

--- flockwave/spec/version.py
+++ flockwave/spec/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave protocol specification."""
 
-__version_info__ = (0, 20, 0)
+__version_info__ = (0, 20, 1)
 __version__ = ".".join("{0}".format(v) for v in __version_info__)
