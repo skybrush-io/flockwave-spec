8a06a6210e69ca838418c2c085c1da7c74dfa535
Tamas Nepusz <ntamas@gmail.com>
2021-06-25 23:23:53 +0200

    chore: bumped version to 1.49.0

--- src/flockwave/spec/version.py
+++ src/flockwave/spec/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave protocol specification."""
 
-__version_info__ = (1, 48, 0)
+__version_info__ = (1, 49, 0)
 __version__ = ".".join("{0}".format(v) for v in __version_info__)
