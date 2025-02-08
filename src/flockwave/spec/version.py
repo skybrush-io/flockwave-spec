595d52b5df1f0451ca32dfca421850212fa4c0cf
Tamas Nepusz <ntamas@gmail.com>
2025-02-08 23:59:55 +0100

    chore: bumped version to 2.3.0

--- src/flockwave/spec/version.py
+++ src/flockwave/spec/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave protocol specification."""
 
-__version_info__ = (2, 2, 0)
+__version_info__ = (2, 3, 0)
 __version__ = ".".join("{0}".format(v) for v in __version_info__)
